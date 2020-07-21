a844e4c0fdb21c1a4ff3a08333bc280a367525b4
S. Steven Reyes <s.steven.reyes@gmail.com>
2020-07-20 21:16:15 -0400

    Created MeasurementTable comp and started table responsive styling

--- shirtspec-app/src/components/MeasurementReview/MeasurementReview.js
+++ shirtspec-app/src/components/MeasurementReview/MeasurementReview.js
@@ -6,6 +6,7 @@ import Content from '../Content';
 import Buttons from '../Buttons/Buttons'
 import MeasurementsContext from '../../context/MeasurementsContext'
 import ShirtspecApiService from '../../services/api-endpoint-service'
+import MeasurementTable from '../MeasurementTable/MeasurementTable'
 
 class MeasurementReview extends Component {
     static defaultProps = {
@@ -39,65 +40,11 @@ class MeasurementReview extends Component {
         return (
             <>
                 <Navbar links={links}/>
-                <Content className="review-table">
+                {/* <Content className="review-table"> */}
                     {error ? <p>Customer cannot be retrieved</p>
                     : <h2>{customer.customer_name}</h2>}
-                    <table>
-                        <thead>
-                            <tr>
-                                <td>Chest</td>
-                                <td>Shirt Waist</td>
-                                <td>Yoke</td>
-                                <td>Shaping</td>
-                                <td>Left Sleeve</td>
-                                <td>Right Sleeve</td>
-                                <td>Left Cuff</td>
-                                <td>Right Cuff</td>
-                                <td>Tail</td>
-                                <td>Collar</td>
-                                <td>Shoulder Line</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                {/* TODO: loop */}
-                                <td>
-                                    <span id="chest">{customer.chest}</span>
-                                </td>
-                                <td>
-                                    <span id="shirt-waist">{customer.shirt_waist}</span>
-                                </td>
-                                <td>
-                                    <span id="yoke">{customer.yoke}</span>
-                                </td>
-                                <td>
-                                    <span id="shaping">{customer.shaping}</span>
-                                </td>
-                                <td>
-                                    <span id="left-sleeve">{customer.left_sleeve}</span>
-                                </td>
-                                <td>
-                                    <span id="right-sleeve">{customer.right_sleeve}</span>
-                                </td>
-                                <td>
-                                    <span id="left-cuff">{customer.left_cuff}</span>
-                                </td>
-                                <td>
-                                    <span id="right-cuff">{customer.right_cuff}</span>
-                                </td>
-                                <td>
-                                    <span id="tail">{customer.tail}</span>
-                                </td>
-                                <td>
-                                    <span id="collar">{customer.collar}</span>
-                                </td>
-                                <td>
-                                    <span id="shoulder-line">{customer.shoulder_line}</span>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </Content>
+                    <MeasurementTable customer={customer} className='existing-customer'/>
+                {/* </Content> */}
                 <Buttons
                     className="MeasurementReview"
                     tag={Link}
---
+++ shirtspec-app/src/components/MeasurementTable/MeasurementTable.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './MeasurementTable.css'
+
+    function MeasurementTable({ customer, className }) {
+        return (
+            <table className={['responsive-table', className].join(' ')}>
+                <thead>
+                    <tr>
+                        <th>Chest</th>
+                        <th>Shirt Waist</th>
+                        <th>Yoke</th>
+                        <th>Shaping</th>
+                        <th>Left Sleeve</th>
+                        <th>Right Sleeve</th>
+                        <th>Left Cuff</th>
+                        <th>Right Cuff</th>
+                        <th>Tail</th>
+                        <th>Collar</th>
+                        <th>Shoulder Line</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td data-label='chest' id="chest">{customer.chest || ''}</td>
+                        <td data-label='shirt waist' id="shirt-waist">{customer.shirt_waist || ''}</td>
+                        <td data-label='yoke' id="yoke">{customer.yoke || ''}</td>
+                        <td data-label='shaping' id="shaping">{customer.shaping || ''}</td>
+                        <td data-label='left sleeve' id="left-sleeve">{customer.left_sleeve || ''}</td>
+                        <td data-label='right sleeve' id="right-sleeve">{customer.right_sleeve || ''}</td>
+                        <td data-label='left cuff' id="left-cuff">{customer.left_cuff || ''}</td>
+                        <td data-label='right cuff' id="right-cuff">{customer.right_cuff || ''}</td>
+                        <td data-label='tail' id="tail">{customer.tail || ''}</td>
+                        <td data-label='collar' id="collar">{customer.collar || ''}</td>
+                        <td data-label='shoulder line' id="shoulder-line">{customer.shoulder_line || ''}</td>
+                    </tr>
+                </tbody>
+            </table>
+        );
+    }
+
+export default MeasurementTable;
